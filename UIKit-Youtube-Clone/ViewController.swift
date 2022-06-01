b4dfcf7542d2fabc9adfa88e4f033fbb73ccc47b
Yılmaz Edis <ylmazedis@gmail.com>
2022-06-01 23:52:24 +0300

    Initial Commit

---
+++ UIKit-Youtube-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Youtube-Clone
+//
+//  Created by yilmaz on 1.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
