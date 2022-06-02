f28d8d5da6326bdf84e5c208c5ac32f4cd70c74e
Yılmaz Edis <ylmazedis@gmail.com>
2022-06-03 01:40:20 +0300

    Added custom half view to create button

--- UIKit-Youtube-Clone/Views/TabBarViewController.swift
+++ UIKit-Youtube-Clone/Views/TabBarViewController.swift
@@ -8,26 +8,31 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        
+        delegate = self
 
         let vc1 = UINavigationController(rootViewController: HomeViewController())
         let vc2 = UINavigationController(rootViewController: ShortsViewController())
-        let vc3 = UINavigationController(rootViewController: SubscriptionsViewController())
-        let vc4 = UINavigationController(rootViewController: LibraryViewController())
+        let vc3 = UINavigationController(rootViewController: CreateViewController())
+        let vc4 = UINavigationController(rootViewController: SubscriptionsViewController())
+        let vc5 = UINavigationController(rootViewController: LibraryViewController())
 
         vc1.tabBarItem.image = UIImage(named: "Home")
         vc2.tabBarItem.image = UIImage(named: "Shorts")
-        vc3.tabBarItem.image = UIImage(named: "Subscriptions")
-        vc4.tabBarItem.image = UIImage(named: "Library")
+        vc3.tabBarItem.image = UIImage(systemName: "plus.circle")
+        vc4.tabBarItem.image = UIImage(named: "Subscriptions")
+        vc5.tabBarItem.image = UIImage(named: "Library")
 
         vc1.title = "Home"
         vc2.title = "Shorts"
-        vc3.title = "Subscriptions"
-        vc4.title = "Library"
-        
-        //tabBar.tintColor = UIColor(named: "NavBarColor") ?? .systemBlue
-                
+        vc4.title = "Subscriptions"
+        vc5.title = "Library"
+               
         let appearance = tabBar.standardAppearance
         appearance.configureWithDefaultBackground()
         //appearance.backgroundColor = UIColor(named: "NavBarColor") ?? .systemBlue
@@ -36,8 +41,31 @@ class TabBarViewController: UITabBarController {
         } else {
             tabBar.standardAppearance = appearance
         }
-        
-        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+
+        setViewControllers([vc1, vc2, vc3, vc4, vc5], animated: true)
     }
+    
+
 }
 
+
+// MARK: - UITabBarController Delegate
+extension TabBarViewController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        guard let selectedIndex = tabBarController.viewControllers?.firstIndex(of: viewController) else {
+            return true
+        }
+        
+        // Your middle tab bar item index.
+        // In my case it's 1.
+        if selectedIndex == 2 {
+            
+            let viewController = CreateViewController()
+            present(viewController, animated: true, completion: nil)
+            
+            return false
+        }
+        
+        return true
+    }
+}
